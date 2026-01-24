e5947b56e2ded8fda18d0973229ddae7d467da9d
RusiruB <rusirubandara2003@gmail.com>
2026-01-25 00:27:47 +0530

    chat-client.js file added

--- public/views/chat.js
+++ public/views/chat.js
@@ -1,7 +1,20 @@
 export function chatInit() {}
 
 export default function chat() {
-  return `<div>chat</div>`
+  return ` 
+  <div>
+    <h1><img src=/images/AI.png width="40" height="40"> AI Chatbot<h1>
+  </div>
+   <div class="chat-container">
+      <div id="chat-box" class="chat-box">
+        <div class="bot-msg">👋 Hi! I'm your StudyBuddy AI🎓✨. Ask me anything..</div>
+      </div>
+
+      <div class="chat-input">
+        <input type="text" id="userInput" placeholder="Type your message..." />
+        <button id="sendBtn">Send</button>
+      </div>
+    </div>`
 }
 
 
