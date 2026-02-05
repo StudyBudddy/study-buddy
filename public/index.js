b37556cdc5d04fdc9571b8947ce90b72cfe6a4bd
hameezexe <hameezexe@gmail.com>
2026-02-06 00:36:07 +0530

    Added account information display

--- public/index.js
+++ public/index.js
@@ -2,6 +2,40 @@ const toggle = document.getElementById("themeToggle");
 const root = document.documentElement;
 const logo = document.getElementById("logo");
 
+const userProfile = document.getElementById("user-profile");
+const loginBtn = document.getElementById("login-btn");
+const userNameTxt = document.getElementById("user-name");
+const userEmailTxt = document.getElementById("user-email");
+
+function updateAuthUI() {
+  const token = localStorage.getItem("token");
+  const userString = localStorage.getItem("user");
+
+  if (token && userString) {
+    const user = JSON.parse(userString);
+
+    userNameTxt.textContent = user.username;
+    userEmailTxt.textContent = user.email;
+
+    loginBtn.classList.add("hidden");
+    userProfile.classList.remove("hidden");
+  } else {
+    loginBtn.classList.remove("hidden");
+    userProfile.classList.add("hidden");
+  }
+}
+
+userProfile.addEventListener("click", () => {
+  const confirmLogout = confirm("Do you want to log out?");
+  if (confirmLogout) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/login";
+  }
+});
+
+updateAuthUI();
+
 function updateLogo(theme) {
   logo.src =
     theme === "light" ? "./images/logo-light.png" : "./images/logo.png";
